9a41494a86b3d4df9beb68fcd53157faeb9a7745
hgsu4nh <natan.mishali@gmail.com>
2026-09-20 12:07:20 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE = "formfit-v3";
-self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./"]))); });
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", e => {
-  if (e.request.method === "GET") e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
